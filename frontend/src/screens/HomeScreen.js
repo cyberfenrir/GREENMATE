8e79b19f5389bfd93982149338544e38b9310010
Siddhant Maitra <smaitra.dev@gmail.com>
2022-11-20 00:43:41 +0530

    render products using axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import products from '../products.js'
+import React, {useState, useEffect} from 'react'
+//import products from '../products.js'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/product'
+import axios from 'axios'
+
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+     async function fetchProducts(){
+      const {data} = await axios.get('http://127.0.0.1:8000/api/products/')
+      setProducts(data)
+    }
+
+    fetchProducts()
+    
+  },[] )
   return (
+    
     <div>
       <h1>Products</h1>
       <Row>
